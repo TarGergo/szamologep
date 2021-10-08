330dcfbbdf288f8f70a4af5f9f7c8e681fefdf92
freasyxd <1999gergo@gmail.com>
2021-10-08 10:26:05 +0200

    calculator app

--- calculator_arg.py
+++ calculator_arg.py
@@ -2,9 +2,36 @@ import sys
 
 import secret_logic
 
-sys.argv
+print(f"{sys.argv}, {len(sys.argv)}")
 
+def printusageandexit():
+    print("Incorrect syntax")
+    exit(-1)
 
-result = secret_logic.calculate(op1, operator, op2)
+def process_one_row(row):
+    parts = row.split()
+    if len(parts) != 3:
+        printusageandexit()
+
+    if not secret_logic.is_numeric(parts[0]):
+        printusageandexit()
+    op1 = int(parts[0])
+
+    if not secret_logic.is_supported(parts[1]):
+        printusageandexit()
+    operator = parts[1]
+
+    if not secret_logic.is_numeric(parts[2]):
+        printusageandexit()
+    op2 = int(parts[2])
+
+    return op1, operator, op2
+
+def getinputs():
+    for row in sys.stdin:
+        process_one_row(row)
+
+op11, operator1, op22 = getinputs()
+result = secret_logic.calculate(op11, operator1, op22)
 print(f"Results: {result}")
 exit(0)
